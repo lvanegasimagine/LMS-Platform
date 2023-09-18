80b593e62503db7fd4039fe88a386908e9b73c09
lvanegasimagine <lvanegas1429@gmail.com>
2023-09-18 13:57:47 -0600

    Folders Setup done

---
+++ app/(auth)/(routes)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignInPage = () => {
+    return (
+        <p className='text-3xl font-medium text-sky-950 mt-10 px-10'>This is a SignIn Page</p>
+    )
+}
+
+export default SignInPage
---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <p className='text-3xl font-medium text-sky-950 mt-10 px-10'>This is a Protected Page</p>
+    )
+}
+
+export default Home
