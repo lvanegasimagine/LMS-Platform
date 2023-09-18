df4e0b77ef3da9bd83c7bd07f743206abf829409
lvanegasimagine <lvanegas1429@gmail.com>
2023-09-18 16:34:10 -0600

    Dynamic layout mode teacher done

--- app/(dashboard)/_components/Sidebar-routes.tsx
+++ app/(dashboard)/_components/Sidebar-routes.tsx
@@ -1,19 +1,26 @@
 'use client'
-import { Layout, Compass } from 'lucide-react'
-import React from 'react'
+import { usePathname } from 'next/navigation'
+import { Layout, Compass, List, BarChart } from 'lucide-react'
 import { SidebarItem } from './Sidebar-Item'
 
 const guestRoutes = [
     { icon: Layout, label: "Dashboard", href: '/' },
     { icon: Compass, label: "Browse", href: '/search' },
 ]
+
+const teacherRoutes = [
+    { icon: List, label: "Courses", href: '/teacher/courses' },
+    { icon: BarChart, label: "Analytics", href: '/teacher/analytics' },
+]
 export const SidebarRoutes = () => {
-    const routes = guestRoutes
+    const pathname = usePathname()
+    const isTeacherPage = pathname?.includes("/teacher")
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes
 
     return (
         <div className='flex flex-col w-full'>
             {routes.map((route) => (
-                <SidebarItem key={route.href} icon={route.icon} label={route.label} href={route.href}/>
+                <SidebarItem key={route.href} icon={route.icon} label={route.label} href={route.href} />
             ))}
         </div>
     )
