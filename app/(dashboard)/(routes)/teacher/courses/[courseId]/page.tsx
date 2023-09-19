8868ad7bebaa187362a011898afe2c6743d93428
lvanegasimagine <lvanegas1429@gmail.com>
2023-09-19 11:26:37 -0600

    Course title form done

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 import { IconBadge } from '@/components/icon-badge'
 import { LayoutDashboard } from 'lucide-react'
+import TitleForm from './_components/Title-Form'
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth()
@@ -42,9 +43,13 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                 <div className="">
                     <div className='flex items-center gap-x-2'>
-                        <IconBadge icon={LayoutDashboard}/>
+                        <IconBadge icon={LayoutDashboard} />
                         <h2 className='text-xl'>Customize your course</h2>
                     </div>
+                    <TitleForm
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>
