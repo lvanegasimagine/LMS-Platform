c5e434a9cf8e116acac2c8d69534bde42a51dffe
lvanegasimagine <lvanegas1429@gmail.com>
2023-09-26 10:19:54 -0600

    Categories done

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -2,21 +2,39 @@ import { db } from '@/lib/db'
 import React from 'react'
 import { Categories } from './_components'
 import SearchInput from '@/components/SearchInput'
+import { getCourses } from '@/actions/getCourses'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import CoursesList from '@/components/courses-list'
 
-const SearchPage = async () => {
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string
+    }
+}
+
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+    const { userId } = auth()
+
+    if (!userId) return redirect('/')
+
     const categories = await db.category.findMany({
         orderBy: {
             name: 'asc'
         }
     })
 
+    const courses = await getCourses({ userId, ...searchParams})
+
     return (
         <>
             <div className='px-6 pt-6 md:hidden md:mb-0 block'>
                 <SearchInput />
             </div>
-            <div className='p-6'>
+            <div className='p-6 space-y-4'>
                 <Categories items={categories} />
+                <CoursesList items={courses}/>
             </div>
         </>
     )
