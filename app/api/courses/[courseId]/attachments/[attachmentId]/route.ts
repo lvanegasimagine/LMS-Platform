31824d9e8fad80806c26eeaae94c98871e7559ae
lvanegasimagine <lvanegas1429@gmail.com>
2023-09-29 10:12:57 -0600

    Final

--- app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
+++ app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -1,11 +1,12 @@
 import { db } from '@/lib/db';
+import { isTeacher } from '@/lib/teacher';
 import { auth } from '@clerk/nextjs';
 import { NextResponse } from 'next/server';
 export async function DELETE(req: Request, { params }: { params: { courseId: string, attachmentId: string } }) {
     try {
         const { userId } = auth()
 
-        if (!userId) return new NextResponse("Unauthorized", { status: 401 });
+        if (!userId || isTeacher(userId)) return new NextResponse("Unauthorized", { status: 401 });
 
         const courseOwner = await db.course.findUnique({
             where: {
